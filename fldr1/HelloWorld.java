0588ee656652e00fdad91e1936090bf0214ee58d
Namrata Chand <148558743+Namrata-Chand@users.noreply.github.com>
2023-10-26 23:17:22 +0530

    Update HelloWorld.java
    2nd

--- fldr1/HelloWorld.java
+++ fldr1/HelloWorld.java
@@ -3,7 +3,7 @@ package com.ripples.git.HelloWorldProj;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git World...updated once....changed for pull to local");
+		System.out.println("Hello Git World...updated once....changed for pull to local..2nd");
 	}
 
 }
